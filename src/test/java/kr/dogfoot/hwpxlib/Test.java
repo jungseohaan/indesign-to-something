39ae046b6cbf1dbb0b43a643eabaf6329d991175
neolord <neolord@hanmail.net>
2026-01-23 14:19:11 +0900

    XML-attribute을 write할 때, escape 문자 처리..

--- src/test/java/kr/dogfoot/hwpxlib/Test.java
+++
@@ -1,9 +0,0 @@
-package kr.dogfoot.hwpxlib;
-
-import kr.dogfoot.hwpxlib.reader.HWPXReader;
-
-public class Test {
-    public static void main(String[] args) throws Exception {
-        HWPXReader.fromFilepath("testFile/error/표준정기적금규정 개정 전문.hwpx");
-    }
-}
